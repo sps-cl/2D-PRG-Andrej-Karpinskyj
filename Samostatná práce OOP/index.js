090f6254efd3ab9a8a6cbdd613518c43d318ae4a
Andriyyj <144383057+Andriyyj@users.noreply.github.com>
2023-10-19 11:39:47 +0200

    Update index.js

--- Samostatná práce OOP/index.js
+++ Samostatná práce OOP/index.js
@@ -1,18 +1,24 @@
-let scene = new Scene(1000,700);
+let scene = new Scene(1100, 700);
 let background = new Background(scene.width, scene.height);
-scene.draw(background);
+let score = new Score(new Vector(scene.width / 2, 50), "Arial", 30);
+let player = new Player(new Vector(100, 0), 150);
+let obstacle1 = randomObstacle(scene.width);
+let obstacle2 = randomObstacle(scene.width * 1.5);
 
-let player = new Player(new Vector(100,0),150);
-let meteor = new Meteor(new Vector(100,0),new Vector(-1.4, 0), 150);
-let cactus = new Cactus(new Vector(100,0),new Vector(-1.4, 0), 150, 150);
-scene.draw(background, meteor, player, cactus)
+player.maxBounds = new Vector(scene.width, scene.height * 0.87);
 
-let circle1 = new CircleCollider(new Vector(0, 0), 50);
-let circle2 = new CircleCollider(new Vector(50, 50), 70);
-let rect1 = new RectCollider(new Vector(110, 110), 50, 100);
-let rect2 = new RectCollider(new Vector(150, 150), 100, 50);
+function randomObstacle(xPos) {
+    if (Math.random > 0.5) {
+        var obstacle = new Meteor(new Vector(xPos, scene.height * 0.5), new Vector(-5, 0), 140);
+    } else {
+        var obstacle = new Cactus(new Vector(xPos, scene.height * 0.7), new Vector(-5, 0), 100, 100);
+    }
 
-console.log(circle1.collideWith(circle2));
-console.log(rect1.collideWith(rect2));
-console.log(circle2.collideWith(rect1));
-console.log(rect1.collideWith(circle1));
+    obstacle.minBounds = new Vector(0,0);
+    obstacle.maxBounds = new Vector(scene.width, scene.height);
+    return obstacle;
+}
+
+function onObstacle2Exit() {
+    
+}
