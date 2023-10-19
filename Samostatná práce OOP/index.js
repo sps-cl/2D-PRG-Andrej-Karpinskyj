444368d6ca3466f0b1d87d81cb6ee61872e723a1
Andriyyj <144383057+Andriyyj@users.noreply.github.com>
2023-10-19 16:16:48 +0200

    Update index.js

--- Samostatná práce OOP/index.js
+++ Samostatná práce OOP/index.js
@@ -1,9 +1,9 @@
-let scene = new Scene(1100, 700);
 let background = new Background(scene.width, scene.height);
 let score = new Score(new Vector(scene.width / 2, 50), "Arial", 30);
 let player = new Player(new Vector(100, 0), 150);
 let obstacle1 = randomObstacle(scene.width);
 let obstacle2 = randomObstacle(scene.width * 1.5);
+let keys =  [];
 
 player.maxBounds = new Vector(scene.width, scene.height * 0.87);
 
@@ -19,6 +19,31 @@ function randomObstacle(xPos) {
     return obstacle;
 }
 
-function onObstacle2Exit() {
-    
+function onObstacle1Exit() {
+    obstacle1 = randomObstacle(scene.width);
+    obstacle2.onSceneExit = onObstacle2Exit;
 }
+
+function onObstacle2Exit() {
+    obstacle2 = randomObstacle(scene.width);
+    obstacle2.onSceneExit =  onObstacle2Exit;
+
+}
+
+
+
+function gameLoop() {
+
+
+    if (keys["s"]) player.crouch();
+    else player.getUp();
+    if (keys["w"]) player.jump()
+
+    
+
+    if
+
+    requestAnimationFrame(draw)
+}
+
+gameLoop();
