c6ca55032ad7430616be7b14a60025b89883cbcd
Andriyyj <144383057+Andriyyj@users.noreply.github.com>
2023-10-19 10:57:15 +0200

    Update index.js

--- Samostatná práce OOP/index.js
+++ Samostatná práce OOP/index.js
@@ -2,8 +2,10 @@ let scene = new Scene(1000,700);
 let background = new Background(scene.width, scene.height);
 scene.draw(background);
 
-let player = new Player(new Vector(100,0), 150);
-scene.draw(background, player)
+let player = new Player(new Vector(100,0),150);
+let meteor = new Meteor(new Vector(100,0),new Vector(-1.4, 0), 150);
+let cactus = new Cactus(new Vector(100,0),new Vector(-1.4, 0), 150, 150);
+scene.draw(background, meteor, player, cactus)
 
 let circle1 = new CircleCollider(new Vector(0, 0), 50);
 let circle2 = new CircleCollider(new Vector(50, 50), 70);
