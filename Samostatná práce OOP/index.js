141f12b8f8c8052514bc8d4b0f6b2c1b8bab8928
Andriyyj <144383057+Andriyyj@users.noreply.github.com>
2023-11-04 14:22:36 +0100

    Update index.js
    kód dokončen

--- Samostatná práce OOP/index.js
+++ Samostatná práce OOP/index.js
@@ -1,50 +1,50 @@
-let scene = new Scene(1100, 700);
+let scene = new Scene( 1100, 700);
 let background = new Background(scene.width, scene.height);
 let score = new Score(new Vector(scene.width / 2, 50), "Arial", 30);
 let player = new Player(new Vector(100, 0), 150);
-let obstacle1 = randomObstacle(scene.width);
-let obstacle2 = randomObstacle(scene.width * 1.5);
-let keys =  [];
-
 player.maxBounds = new Vector(scene.width, scene.height * 0.87);
-
-function randomObstacle(xPos) {
-    if (Math.random > 0.5) {
-        var obstacle = new Meteor(new Vector(xPos, scene.height * 0.5), new Vector(-5, 0), 140);
+function randomObstacle(xPos){
+    if(Math.random() > 0.5){
+        var obstacle = new Meteor(new Vector(xPos, scene.height * 0.5), new Vector(-5, 0),140);
     } else {
         var obstacle = new Cactus(new Vector(xPos, scene.height * 0.7), new Vector(-5, 0), 100, 100);
     }
-
-    obstacle.minBounds = new Vector(0,0);
-    obstacle.maxBounds = new Vector(scene.width, scene.height);
-    return obstacle;
+    obstacle.minBounds = (new Vector(0, 0));
+    obstacle.maxBounds = (new Vector(scene.width,scene.height));
+    return obstacle
 }
 
-function onObstacle1Exit() {
+let obstacle1 = randomObstacle(scene.width);
+let obstacle2 = randomObstacle(scene.width * 1.5);
+
+function onObstacle1Exit(){
     obstacle1 = randomObstacle(scene.width);
     obstacle1.onSceneExit = onObstacle1Exit;
 }
 
-function onObstacle2Exit() {
+function onObstacle2Exit(){
     obstacle2 = randomObstacle(scene.width);
-    obstacle2.onSceneExit =  onObstacle2Exit;
-
+    obstacle2.onSceneExit = onObstacle2Exit;
 }
 
 obstacle1.onSceneExit = onObstacle1Exit;
 obstacle2.onSceneExit = onObstacle2Exit;
 
-document.onkeydown = function(event) {
+let keys = [];
+
+document.onkeydown = onKeyDown;
+function onKeyDown(event){
     keys[event.key] = true;
 }
 
-document.onkeyup = function(event) {
-    keys[event.key] = false;
+document.onkeyup = onkeyup;
+function onkeyup(event){
+    keys[event.key] = false
 }
 
-function gameLoop() {
+function gameLoop(){
     score.increase();
-    
+
     if (keys["s"]) player.crouch();
     else player.getUp();
     if (keys["w"]) player.jump();
@@ -52,13 +52,11 @@ function gameLoop() {
     player.move();
     obstacle1.move();
     obstacle2.move();
-    
     scene.draw(background, player, obstacle1, obstacle2, score);
-
-    if(player.collideWith(obstacle1) || player.collideWith(obstacle2))
-    return;
-
-    requestAnimationFrame(gameLoop);
+    if(player.collideWith(obstacle1)|| player.collideWith(obstacle2)){
+        return
+    }
+    requestAnimationFrame(gameLoop)
 }
 
 gameLoop();
