c695898861ef2660e694726025aa55518333598c
Andriyyj <144383057+Andriyyj@users.noreply.github.com>
2023-12-13 18:15:54 +0100

    Update insertionSortAnim.js

--- Řadící algoritmy/insertionSortAnim.js
+++ Řadící algoritmy/insertionSortAnim.js
@@ -1,5 +1,16 @@
-class InsertionSort extends sortAnim {
-    constructor(values, container) {
-
+class InsertionSortAnim extends sortAnim {
+    async sortAsc() {
+        for (let i = 1; i < this.values.length; i++) {
+            let j = i - 1;
+            while(j >= 0 && this.values[j] > this.values[j + 1]) {
+                this.setCompareColor(this.columns[j + 1]);
+                this.setCompareColor(this.columns[j]);
+                await this.sleep(500);
+                this.setDefaultColor(this.columns[j + 1]);
+                this.setDefaultColor(this.columns[j]);
+                this.swapValues(j, j+ 1);
+                j--;
+            }
+        }
     }
 }
