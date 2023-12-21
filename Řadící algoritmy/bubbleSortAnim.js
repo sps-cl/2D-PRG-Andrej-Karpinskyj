962af7c035d8040b779a69a13d967957ba92b83c
Andriyyj <144383057+Andriyyj@users.noreply.github.com>
2023-12-21 11:28:29 +0100

    Update bubbleSortAnim.js

--- Řadící algoritmy/bubbleSortAnim.js
+++ Řadící algoritmy/bubbleSortAnim.js
@@ -1,22 +1,26 @@
 class BubbleSortAnim extends SortAnim{
-    
-    async sortAsc() {
-        for (let i = 0; i < this.values.length; i++) {
+
+    async sortAsc(){
+        for (var i = 0; i < this.values.length; i++) {
             let swap = false;
             let bound = this.values.length - i;
             for (let j = 1; j < bound; j++) {
                 this.setCompareColor(this.columns[j - 1]);
                 this.setCompareColor(this.columns[j]);
-                await this.sleep(500);
+                await this.sleep(50);
                 this.setDefaultColor(this.columns[j - 1]);
                 this.setDefaultColor(this.columns[j]);
-                if (this.values[j - 1] > this.values[j]) {
+                if (this.values[j -1] > this.values[j]){
                     swap = true;
                     this.swapValues(j - 1, j);
                 }
             }
             this.setSortedColor(this.columns[bound - 1]);
-            if (!swap) return;
+            if(!swap) break;
+        }
+        for (let index = 0; index < i; index++) {
+            this.setSortedColor(this.columns[index])
+            
         }
     }
 }
