89ffe2e6673f9e53cd9c8edfed5141091d7efbfe
Andriyyj <144383057+Andriyyj@users.noreply.github.com>
2023-12-21 11:17:05 +0100

    Update shellSortAnim.js

--- Řadící algoritmy/shellSortAnim.js
+++ Řadící algoritmy/shellSortAnim.js
@@ -1,18 +1,22 @@
-class ShellSort extends sortAnim{
-
+class ShellSortAnim extends SortAnim {
+    
     async sortAsc() {
-        for (let gap = this.array.length >> 1; gap > 0; gap >>= 1) {
-            for (let i = gap; i < this.array.length; i++) {
-                this.setCompareColor(this.columns[j / 2]);
-                this.setCompareColor(this.columns[j]);
-                let j = i - gap;
-                this.setDefaultColor(this.columns[j / 2]);
-                this.setDefaultColor(this.columns[j]);
-                await this.sleep(500);
-                while(j>=0 && this.arrayOfValues[j] > value) {
-                    this.setCompareColor(j);
+            for (var gap = this.values.length>>1; gap > 0; gap >>= 1) {
+                for (var i = gap; i < this.values.length; i++){
+                    let j = i -gap;
+                    while(j>= 0 && this.values[j] > this.values[j + gap]) {
+                        this.setCompareColor(this.columns[j]);
+                        this.setCompareColor(this.columns[j+ gap]);
+                        await this.sleep(500);
+                        this.setDefaultColor(this.columns[j]);
+                        this.setDefaultColor(this.columns[j+ gap]);
+                        this.swapValues(j,j + gap)
+                        j-= gap;
+                    }
                 }
-            }
+            }   
+        for (let index = 0; index < i; index++) {
+                this.setSortedColor(this.columns[index]);
         }
     }
 }
