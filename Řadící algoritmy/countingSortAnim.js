4b136ee631531bff05b0f0653470f56fd941babc
Andriyyj <144383057+Andriyyj@users.noreply.github.com>
2024-01-17 08:47:34 +0100

    Add files via upload

---
+++ Řadící algoritmy/countingSortAnim.js
@@ -0,0 +1,12 @@
+class CountingSortAnim extends SortAnim{
+
+    async sortAsc() {
+        
+
+
+
+        for (let index = 0; index < i; index++) {
+        this.setSortedColor(this.columns[index]);
+        }
+    }
+}
